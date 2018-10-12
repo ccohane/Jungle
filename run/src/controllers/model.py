03d7fe061dc73a273c0c2dafd02be0d854288884
Callahan Cohane <CalCohane@Callahans-MacBook-Air.local>
2018-10-12 14:27:31 -0400

    Created a Login, register, and basic dashboard html pages for Jungle, Feed the beast

--- run/src/controllers/model.py
+++ run/src/controllers/model.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
 
 
+def create_user(user_name , password):
+
+    with Database() as db:
+        create_table()
+        user_name_taken = check_user_exist(user_name)
+        if user_name_taken==False:
+            sql='''INSERT INTO users(user_names,password) 
+                            Values(?,?);'''
+            db.c.execute(sql, (user_name, password))
+            return True
+        else:
+            return False
+
+def check_user_exist(user_name):
+    with Database() as db:
+        db.c.execute('''SELECT * FROM users WHERE user_names='{}';'''.format(user_name))
+        result=db.c.fetchone()
+
+        if result:
+            return True
+        else:
+            return False
+
+def check_user(user_name, password):
+    with Database() as db:
+        db.c.execute('''SELECT * FROM users WHERE user_names='{}'
+                        AND password='{}';'''.format(user_name, password))
+        result=db.c.fetchone()
+
+        if result:
+            return True
+        else:
+            return False
